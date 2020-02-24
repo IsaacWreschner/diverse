6e22fc2bd0fac682459fefe4bfadcb8857f30608
BenZionKatz <ben8652800@gmail.com>
2020-02-24 13:12:23 +0200

    Create classBid.js

---
+++ classBid.js
@@ -0,0 +1,11 @@
+class CQuotation{
+    constructor(){
+        this.quot = {
+            "componyName" : String,
+            "price"       : Number,
+            "rderId"      : Number
+        }
+        this.error = false;
+    }
+}
+module.exports = {CQuotation}
